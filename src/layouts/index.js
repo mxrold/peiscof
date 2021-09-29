769e79373556ee70edd01ebfb93d7982f51f0f62
Martin <martinroldan889@gmail.com>
2021-09-28 22:01:02 -0300

    :ambulance: Critical hotfix build

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,18 @@
 import advanced from './advanced';
 import blog from './blog';
 import post from './post';
+import page from './page';
 
 export {
     advanced,
     blog,
     post,
+    page
 };
 
 export default {
     advanced,
     blog,
     post,
+    page
 };
