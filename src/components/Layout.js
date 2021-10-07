37da62eaff356ced7833575be3f006fc8ba797b4
Martin <martinroldan889@gmail.com>
2021-10-07 19:45:28 -0300

    :children_crossing: Changed family font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -72,7 +72,9 @@ export default class Body extends React.Component {
                     <meta name="description" content={seoDescription} />
                     {!_.isEmpty(seoRobots) && <meta name="robots" content={seoRobots} />}
                     {seoExtra}
-                    <link href="https://fonts.googleapis.com/css?family=Karla:400,400i,500,700,700i&display=swap" rel="stylesheet" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,400;1,700&display=swap" rel="stylesheet" />
                     {favicon && <link rel="icon" href={withPrefix(favicon)} />}
                     <body className={classNames(`palette-${colorScheme}`, `accent-${accentColor}`)} />
                 </Helmet>
