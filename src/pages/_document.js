1fe5f3eaa9b0193a819e44b68d8ae006185698dc
Martin <martinroldan889@gmail.com>
2021-10-08 14:41:24 -0300

    :chart_with_upwards_trend: Google analytics added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,6 +33,14 @@ export default class MyDocument extends Document {
         return (
             <Html {...this.helmetHtmlAttrComponents}>
                 <Head>{this.helmetHeadComponents}</Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-6WEK5PFW7Z"></script>
+                <script dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+    
+                    gtag('config', 'G-6WEK5PFW7Z');`
+                }} />
                 <body {...this.helmetBodyAttrComponents}>
                     <Main />
                     <script src={withPrefix('js/plugins.js')} />
